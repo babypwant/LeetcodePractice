b14cb4ba56fbeb23e09384cb11298f0b189d9273
Gary Rios <78942879+babypwant@users.noreply.github.com>
2021-08-12 20:33:15 -0700

    leetcode practice

--- api.js
+++ api.js
@@ -1,45 +1,45 @@
-// Symbol       Value
-// I             1
-// V             5
-// X             10
-// L             50
-// C             100
-// D             500
-// M             1000
+// Write a function to find the longest common prefix string amongst an array of strings.
 
-//IM
+// If there is no common prefix, return an empty string "".
 
-/**
- * @param {string} s
- * @return {number}
- */
- var romanToInt = function(s) {
-    for (let  i = 0; i <= s.length; i++){
-    let curr = s[i];
-    let next = s[i+1];
-    if (curr > next){
-        let num = romanToInt(s.slice(0, i));
-        let roman = s.slice(i, s.length);
-        if (roman === "CM"){
-            num += 900;
+
+
+// Example 1:
+
+// Input: strs = ["flower","flow","flight"]
+// Output: "fl"
+// Example 2:
+
+// Input: strs = ["dog","racecar","car"]
+// Output: ""
+// Explanation: There is no common prefix among the input strings.
+
+
+// Constraints:
+
+// 1 <= strs.length <= 200
+// 0 <= strs[i].length <= 200
+// strs[i] consists of only lower-case English letters.
+
+var longestCommonPrefix = function (strs) {
+    let prefix = '';
+    if (strs.length === 0) {
+        return prefix;
+    }
+    let min = strs[0].length;
+    for (let i = 1; i < strs.length; i++) {
+        if (strs[i].length < min) {
+            min = strs[i].length;
         }
-        else if (roman === "CD"){
-            num += 400;
+    }
+    for (let i = 0; i < min; i++) {
+        let ch = strs[0][i];
+        for (let j = 1; j < strs.length; j++) {
+            if (strs[j][i] !== ch) {
+                return prefix;
+            }
         }
-        else if (roman === "XC"){
-            num += 90;
-        }
-        else if (roman === "XL"){
-            num += 40;
-        }
-        else if (roman === "IX"){
-            num += 9;
-        }
-        else if (roman === "IV"){
-            num += 4;
-        }
-        return num;
+        prefix += ch;
     }
 
-    }
 };
