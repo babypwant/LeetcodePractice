7c87ffe4bb3d87ea45a3f28ae6562655455d491d
Gary Rios <78942879+babypwant@users.noreply.github.com>
2023-06-26 14:15:37 -0700

    finished

--- 1.js
+++ 1.js
@@ -1,13 +1,25 @@
+function recursiveBubbleSort (arr, arr_length) { 
 
+//well then I guess I would start by iterationg through the array to check the weights
 
+    for(let i =0; i < arr_length - 1; i++) {
 
-function sortArray (arr, length) { 
+        if (arr[i] > arr[i + 1]){
+            const temp = arr[i]
+            arr[i] = arr[i + 1]
+            arr[ i + 1 ] = temp
+        }
 
+    }
+    
+    if (arr_length > 1) {
+        recursiveBubbleSort(arr, arr_length - 1)
+    }
 
+    return arr
 }
 
-
-sortArray(arr = [
+console.log(recursiveBubbleSort(arr = [
     2,5,7,9,12,18,19,27,13
-],
-arr.length )
+], arr.length))
+
