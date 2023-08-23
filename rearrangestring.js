a92c9511e1b380f440d3fefeba9959c06d6bc44e
Gary Rios <78942879+babypwant@users.noreply.github.com>
2023-08-22 22:37:36 -0700

    Create rearrangestring.js

---
+++ rearrangestring.js
@@ -0,0 +1,50 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reorganizeString = function(s) {
+    
+    // Step 1: Count the frequency of each character
+    const freqMap = new Map();
+    for (let char of s) {
+        freqMap.set(char, (freqMap.get(char) || 0) + 1);
+    }
+
+    // Step 2: Sort characters by frequency
+    const sortedChars = [...freqMap.entries()].sort((a, b) => b[1] - a[1]);
+
+    const result = [];
+    let prevChar = null;
+
+    while (sortedChars.length) {
+        let found = false;
+
+        for (let i = 0; i < sortedChars.length; i++) {
+            const [char, freq] = sortedChars[i];
+
+            // If the current char is not the same as the previous char or it's the last choice
+            if (char !== prevChar || i === sortedChars.length - 1) {
+                found = true;
+                result.push(char);
+                if (freq === 1) {
+                    sortedChars.splice(i, 1); // Remove from the list if frequency is 1
+                } else {
+                    sortedChars[i][1]--; // Decrease the frequency
+                }
+                prevChar = char;
+                break;
+            }
+        }
+
+        // If no valid character found
+        if (!found) {
+            return "";
+        }
+
+        // Sort again for next iteration
+        sortedChars.sort((a, b) => b[1] - a[1]);
+    }
+
+    return result.join('');
+}
+
